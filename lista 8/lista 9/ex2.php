9b7824cd193579cc0574e3592b13fb2c469fa998
Nicolas Campos <n.campos@edu.senai.br>
2026-09-09 10:44:30 -0300

    Add files via upload

---
+++ lista 8/lista 9/ex2.php
@@ -0,0 +1,9 @@
+<?php
+
+$alunos = ["Marlon", "Maria", "Ana", "Pedro"];
+
+// troque os indices 0, 1, 2, 3 e 4
+echo  $alunos[2];
+
+
+?>
---
+++ lista 8/lista 9/ex6.php
@@ -0,0 +1,10 @@
+<?php
+
+$musicas = ["Runaway <br>","Tem Café <br>","confident <br>","All Eyez On Me <br>","Poesia Ácustica 2 <br>","Baby Doll"];
+$total = count($musicas);
+
+foreach ($musicas as $music){
+    echo $music;
+}
+
+?>
